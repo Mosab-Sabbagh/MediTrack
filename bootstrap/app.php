b8150279fb93bbce41bb8b3cdd9083187824233c
Mosab Hamdy <msbalsbagh@gmail.com>
2025-08-26 10:02:58 +0300

    create middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'sick' => \App\Http\Middleware\SickMiddleware::class,
+            'doctor' => \App\Http\Middleware\DoctorMiddleware::class,
+            'pharmaceutical' => \App\Http\Middleware\PharmaceuticalMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
